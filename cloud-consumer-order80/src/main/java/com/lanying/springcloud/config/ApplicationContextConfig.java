9c577ba4c575a7882ff6328106bfa11a533bc794
lanying <761562013@qq.com>
2020-11-11 14:31:14 +0800

    SpringCloud Sleuth分布式链路追踪

--- cloud-consumer-order80/src/main/java/com/lanying/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/lanying/springcloud/config/ApplicationContextConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationContextConfig {
     @Bean
-    //@LoadBalanced // 1.赋予RestTemplate负载均衡的能力，轮训访问Provider集群中的每一台机器 2.通过服务名称找到IP
+    @LoadBalanced // 1.赋予RestTemplate负载均衡的能力，轮训访问Provider集群中的每一台机器 2.通过服务名称找到IP
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
--- cloud-consumer-order80/src/main/java/com/lanying/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/lanying/springcloud/controller/OrderController.java
@@ -48,4 +48,10 @@ public class OrderController {
         URI uri = serviceInstance.getUri();
         return  restTemplate.getForObject(uri+"/payment/lb",String.class);
     }
+
+    @GetMapping(value="/consumer/payment/zipkin")
+    public String paymentZipkin() {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/zipkin/",String.class);
+    }
+
 }
